6bcd4c46b0059647f940ef1a91562bbad0cde74e
jbalaraman <james.balaraman@gmail.com>
2023-06-28 17:27:23 -0600

    I am the biggest idiot known to man, I am a collosal waste of space

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (name) {
+    name = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return name.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (name) {
+    name = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return name.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    const newFare = fare => fare*5
+    return newFare;
+}
+
+function fareDoubler(fare) {
+    return fare*2;
+}
+
+function fareTripler(fare) {
+    return fare*3;
+}
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
+}
